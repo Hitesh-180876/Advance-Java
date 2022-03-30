c4921f3ab6901383f8137e9575444f2e53d5a7bf
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-03-30 23:51:35 +0530

    Update LambdaExpressionWithArguments.java

--- LambdaExpressionWithArguments.java
+++ LambdaExpressionWithArguments.java
@@ -1,1 +1,15 @@
 
+@FunctionalInterface
+interface Lambda{
+    public int display(int x, int y);
+}
+
+
+public class MyClass {
+    public static void main(String args[]) {
+        Lambda m =(a, b)->{ return a+b;};
+        int r = m.display(10, 20);
+        System.out.println(r);
+        
+    }
+}
