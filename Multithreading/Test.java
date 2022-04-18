f4e5d550f5d698faa9a0d6036c78ba4ec60b7ecb
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-18 15:16:54 +0530

    Update Test.java

--- Multithreading/Test.java
+++ Multithreading/Test.java
@@ -1,1 +1,27 @@
 
+package multithreading;
+                                                                                 ________________________________________________
+class MyThread extends Thread{                                                          first MyThread run for infinite loop
+	public void run() {  // override the run method of class Thread                  
+		int i=1;
+		while(true) {
+			System.out.println(i+"Hello");
+			i++;
+		}
+	}
+}
+
+public class Test {
+	public static void main(String[] args) {
+		MyThread t = new MyThread();    // create object of MyThread class
+		t.start();   // start the thread using start() method                      _______________________________________________________
+                                                                                   program does'nt stop and continue itself 
+    /* running main method simultaneously */
+		int i=1;
+		while(true) {
+			System.out.println(i+"world");                                          ---------------------------------------------------------------
+			i++;                                                                               main() loop run  for infinite  
+		}
+	}
+
+}
