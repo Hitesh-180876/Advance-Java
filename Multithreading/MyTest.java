b294e616f2d0e039a0a03c981da9e3d2f646d9cc
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-04-18 15:43:52 +0530

    Update MyTest.java

--- Multithreading/MyTest.java
+++ Multithreading/MyTest.java
@@ -1,1 +1,26 @@
+package multithreading;
+
+public class MyTest implements Runnable {   // override run method in Runnable
+	public void run() {
+		int i=1;
+		while(true) {
+			System.out.println(i+"hello");           // Infinite loop
+			i++;
+		}
+	}
+	public static void main(String[] args) {
+		MyTest t = new MyTest();                // creating object of MyTest class
+		Thread th = new Thread(t);             // passing MyTest object into  Thread object 
+    
+		th.start();      // starting the thread
+		
+		int i=1;
+		while(true) {               
+			System.out.println(i+"world");        // Infinite loop
+			i++;
+		}
+		
+	}
+
+}
 
