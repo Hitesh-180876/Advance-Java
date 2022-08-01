ec864b62400fbf5b7e9835f3fc8a1380bf5c98bb
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-08-01 16:58:16 +0530

    Update Lambda.java

--- Lambdas/Lambda.java
+++ Lambdas/Lambda.java
@@ -1,1 +1,26 @@
 
+package lambda;
+
+@FunctionalInterface  // It can have only one method abstract
+interface  MyLambda{
+	void display();
+}
+
+/*
+ * class My implements MyLambda{ public void display() {
+ * System.out.println("Hello Java"); } }
+ */
+public class Lambda {
+	public static void main(String[] args) {
+//		MyLambda m = new My();
+//		m.display();
+//		
+		MyLambda m = () ->{
+			 
+				System.out.println("Hello java");
+			
+		};
+		m.display();
+	}
+
+}
